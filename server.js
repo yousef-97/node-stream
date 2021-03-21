21959a7c0aabe209b48582efbe893ce927a6ee59
yousefAlshun <yousef.alshun@repzoapp.com>
2021-03-21 19:20:07 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const port = 3000;
+io.on('connection', (socket) => {
+    console.log('connected')
+    socket.on('message', (evt) => {
+        socket.broadcast.emit('message', evt)
+    })
+})
+io.on('disconnect', (evt) => {
+    console.log('disconnected')
+})
+http.listen(port, () => console.log(`server listening on port: ${port}`))
---
+++ src/chat-client.js
@@ -0,0 +1,21 @@
+var socket = require('socket.io-client')('http://localhost:3000');
+  const repl = require('repl')
+  const chalk = require('chalk');
+  socket.on('disconnect', function() {
+      socket.emit('disconnect')
+  });
+  socket.on('connect', () => {
+      console.log(chalk.red('=== start chatting ==='))
+  })
+  socket.on('message', (data) => {
+      const { cmd, username } = data;
+      console.log(chalk.green(data));
+
+    //   console.log(chalk.green(username + ': ' + cmd.split('\n')[0]));
+  })
+  repl.start({
+      prompt: '',
+      eval: (cmd) => {
+          socket.send(cmd)
+      }
+  })
